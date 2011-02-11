007d67451aa7cd183906019222eea4f9f147facf
Mirko Vukovic <mirko.vukovic@us.tel.com>
2011-02-11 15:23:29 -0500

    Including refactored grid mapping, sequencing, manpulation, etc

---
+++ mv-grid/grid-mappers.lisp
@@ -0,0 +1,104 @@
+;; Mirko Vukovic
+;; Time-stamp: <2011-02-10 07:15:31 grid-mappers.lisp>
+;; 
+;; Copyright 2011 Mirko Vukovic
+;; Distributed under the terms of the GNU General Public License
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(in-package :mv-grid)
+
+(export '(gmap gsmap reduce-rows reduce-columns))
+
+(defgeneric gmapc (function grid &rest grids)
+  (:documentation
+   "Apply function to successive sets of arguments in which one argument is obtained from each vector.
+  Return the first grid, unmodified.
+
+This map is used for the function's side effects.  It is modeled after
+mapc")
+  (:method ((function function) (vector #+clisp vector #+sbcl mvector)
+	    &rest vectors)
+    (warn "untested")
+    (map-n-grids :sources (cons vector vectors)
+		 :combination-function #'(lambda (&rest args)
+					   (declare (ignore args))
+					   nil)
+		 :destination-specification `((array ,@(dimensions (vector)))
+					      t))
+    vector))
+
+(defun gmap (function grid)
+  "Element-wise Map `function' over `grids' *array-type* and
+*float-type* determine the result type
+
+gmap specializes map-grid to use only the :element-function
+keyword"
+  (map-grid
+   :source grid
+   :element-function function))
+;;   :destination-specification `((,*array-type* ,@(dimensions grid))
+;;						 ,*float-type*))
+
+
+(defun gsmap (function &rest grids)
+  "Element-wise Map `function' over `grids' *array-type* and
+*float-type* determine the result type
+
+gsmap specializes map-n-grids to use only
+the :combination-function keyword"
+  (let ((affis (mapcar #'grid::affi grids))
+	(index 2))
+    (dolist (this-affi (rest affis))
+      (unless (affi:check-conformability (first affis) this-affi)
+	(error "~s~:*~[nil~;st~;nd~;rd~:;th~] grids's affi conflicts with the first grid's affi" index)))
+    (map-n-grids
+     :sources  (mapcar #'list grids affis)
+     :combination-function #'(lambda (&rest args)
+			       (apply function args))
+     :destination-specification `((,*array-type* ,@(dimensions (first grids)))
+						 ,*float-type*))))
+
+
+
+
+(defun reduce-rows (matrix &optional (func #'+))
+  "Return a column vector with each element a result reducing that row
+using `func' (default #'+)"
+  (make-grid `((,*array-type* 1 ,(first (dimensions matrix)))
+		    double-float)
+	     :initial-contents
+	     ;; In order to create a column array, I have to return a
+	     ;; nested list.  This could be a grid bug or a feature.
+	     (list
+	      (iter:iter
+		(iter:for R :matrix-row matrix)
+		(iter:collect
+		    (iter:iter
+		      (iter:for E :vector-element R)
+		      (iter:reducing E by func initial-value 0d0)))))))
+
+
+(defun reduce-columns (matrix &optional (func #'+))
+  "Return a row vector with each element a result of reducing the
+column using `func' (default #'+)"
+  (make-grid `((,*array-type* ,(second (dimensions matrix)))
+		    double-float)
+	     :initial-contents
+	     (iter:iter
+	       (iter:for C :matrix-column matrix)
+	       (iter:collect
+		   (iter:iter
+		     (iter:for E :vector-element C)
+		     (iter:reducing E by func initial-value 0d0))))))
--- mv-grid/grid-utilities-unit-tests.lisp
+++ mv-grid/grid-utilities-unit-tests.lisp
@@ -1,42 +1,43 @@
-(in-package :mv-grid-unit-tests)
+(in-package :mv-grid)
 
 
-(defparameter +array-3-4-double-float+
-  (grid::test-grid-double-float 'grid:foreign-array '(3 4)))
+(defparameter *array-3-4-double-float*
+  (grid::test-grid-double-float *array-type* '(3 4)))
 
-(defparameter +vector-4-double-float+
-  (grid::test-grid-double-float 'grid:foreign-array '(4)))
+(defparameter *vector-4-double-float*
+  (grid::test-grid-double-float *array-type* '(4)))
+
+(defparameter *0-1-2* (grid::make-grid `((,*array-type*) ,*integer-type*)
+			     :initial-contents '(0 1 2)))
+(defparameter *0-2-4* (grid::make-grid `((,*array-type*) ,*integer-type*)
+				      :initial-contents '(0 2 4)))
 
 (defmacro assert-grid-equal (grid1 grid2)
   `(assert-numerical-equal (grid:copy-to ,grid1)
 			   (grid:copy-to ,grid2)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (setf mv-grid:*array-type* 'grid:foreign-array))
+  (setf mv-grid:*array-type* *array-type*))
 
 (define-test indgen
-  (assert-grid-equal #15m(0 1 2)
+  (assert-grid-equal (grid::make-grid `((,*array-type*) ,*integer-type*)
+				      :initial-contents '(0 1 2))
 		     (indgen 3)))
 
 
 (define-test findgen
-  (assert-grid-equal #1m(0d0 1d0 2d0)
+  (assert-grid-equal
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '(0d0 1d0 2d0))
 		     (findgen 3)))
 
 
 (define-test gmap
-  (let ((v1 #15m(0 1 2))
-	(v2 #15m(0 1 2))
-	(v3 #15m(0 2 4))
-	#|(v4 #(0 1))|#)
     (assert-grid-equal
-     v2 (gmap #'identity v1))
+     *0-1-2* (gmap #'identity *0-1-2*))
     (assert-grid-equal
-     v3
-     (gsmap #'+ v1 v2))
-#|    (assert-grid-equal
-     v3
-     (gmap #'+ v1 v4))|#))
+     *0-2-4*
+     (gsmap #'+ *0-1-2* *0-1-2*)))
 
 (define-test match-vec-element
   (let ((vec (findgen 12)))
@@ -56,35 +57,88 @@
      (not (match-vec-element vec #'(lambda (arg)
 				     (>= arg 15)))))))
 
+(define-test matching-indices
+  (assert-equal '(1)
+		(matching-indices *0-1-2* #'(lambda (arg)
+					      (equal arg 1)))))
 (define-test matrify
   ;; convert vec-4 into 2x2 matrix
-  (assert-grid-equal #1m((0d0 1d0) (2d0 3d0))
-		     (matrify +vector-4-double-float+ 2 2))
+  (assert-grid-equal
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '((0d0 1d0) (2d0 3d0)))
+   (matrify *vector-4-double-float* 2 2))
   ;; convert vec-4 into column vector of length 4
-  (assert-grid-equal #1m((0d0 1d0 2d0 3d0))
-		     (matrify +vector-4-double-float+ 1 4))
+  (assert-grid-equal 
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '((0d0 1d0 2d0 3d0)))
+   (matrify *vector-4-double-float* 1 4))
   ;; convert vec 4 into a row vector
-  (assert-grid-equal #1m((0d0) (1d0) (2d0) (3d0))
-		     (matrify +vector-4-double-float+ 4 1)))
+  (assert-grid-equal
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '((0d0) (1d0) (2d0) (3d0)))
+   (matrify *vector-4-double-float* 4 1)))
 
 
 (define-test read-grid
   (with-open-file (stream
-		   #P"/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #+cysshd1 "/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #-cysshd1 "/home/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
 		   :direction :input) 
-    (assert-grid-equal #1m((1d0 2d0 3d0) (4d0 5d0 6d0))
-			    (read-grid '(2 3) stream))))
+    (assert-grid-equal 
+     (grid::make-grid `((,*array-type*) ,*float-type*)
+		      :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0)))
+     (read-grid '(2 3) 't stream))))
 
-(defparameter +array-3-4-double-float+
-  (grid::test-grid-double-float 'grid:foreign-array '(3 4)))
+(define-test read-csv-grid
+  (with-open-file (stream
+		   #+cysshd1 "/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #-cysshd1 "/home/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.csv"
+		   :direction :input) 
+    (assert-grid-equal 
+     (grid::make-grid `((,*array-type*) ,*float-type*)
+		      :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0)))
+     (read-grid '(2 3) 'csv stream)))
+  (with-open-file (stream
+		   #+cysshd1 "/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #-cysshd1 "/home/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.csv"
+		   :direction :input) 
+    (assert-grid-equal 
+     (grid::make-grid `((,*array-type*) ,*float-type*)
+		      :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0)))
+     (read-grid '(nil 3) 'csv stream)))
+  (with-open-file (stream
+		   #+cysshd1 "/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #-cysshd1 "/home/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.csv"
+		   :direction :input) 
+    (assert-grid-equal 
+     (grid::make-grid `((,*array-type*) ,*float-type*)
+		      :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0)))
+     (read-grid '(2 nil) 'csv stream)))
+  (with-open-file (stream
+		   #+cysshd1 "/home/mv/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.txt"
+		   #-cysshd1 "/home/my-software-add-ons/my-lisp/mv-gsll/mv-grid/2d-grid-data.csv"
+		   :direction :input) 
+    (assert-grid-equal 
+     (grid::make-grid `((,*array-type*) ,*float-type*)
+		      :initial-contents '((1d0 2d0 3d0) (4d0 5d0 6d0)))
+     (read-grid '(nil nil) 'csv stream))))
+
+
+
+(defparameter *array-3-4-double-float*
+  (grid::test-grid-double-float *array-type* '(3 4)))
 
 
 
 (define-test reduce-rows/cols
-  (assert-grid-equal #1m(30d0 33d0 36d0 39d0)
-			  (reduce-columns +array-3-4-double-float+))
-  (assert-grid-equal #1m((6d0 46d0 86d0))
-			  (reduce-rows +array-3-4-double-float+)))
+  (assert-grid-equal 
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '(30d0 33d0 36d0 39d0))
+   (reduce-columns *array-3-4-double-float*))
+  (assert-grid-equal 
+   (grid::make-grid `((,*array-type*) ,*float-type*)
+		    :initial-contents '((6d0 46d0 86d0)))
+   (reduce-rows *array-3-4-double-float*)))
 
 
 ;;;; Local variables: 
